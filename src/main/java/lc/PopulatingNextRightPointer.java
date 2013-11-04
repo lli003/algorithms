dec3821e3909cc30ee6d01faac879f9f56fc080e
lli003 <lli003@cs.fiu.edu>
2013-11-04 16:58:21 -0500

    improved Populating Next Right Pointer

--- src/main/java/lc/PopulatingNextRightPointer.java
+++ src/main/java/lc/PopulatingNextRightPointer.java
@@ -1,49 +1,32 @@
 package lc;
 
-import java.util.LinkedList;
-
 import lc.util.TreeLinkNode;
 
 /**
  * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
  * Initially, all next pointers are set to NULL.
+ * 
+ * You may only use constant extra space.
+ * You may assume that it is a perfect binary tree (i.e., all leaves are at the same level, and every parent has two children).
  * @author lli003
  *
  */
 public class PopulatingNextRightPointer {
   
+  /**
+   * use a recursive way
+   * @param root
+   */
   public void connect(TreeLinkNode root){
-    if (root == null)
+    if (root == null || (root.left == null && root.right == null))
       return;
     
-    LinkedList<TreeLinkNode> list = new LinkedList<TreeLinkNode>();
-    LinkedList<TreeLinkNode> newList = new LinkedList<TreeLinkNode>();
+    root.left.next = root.right;
+    if (root.next != null)
+      root.right.next = root.next.left;
     
-    list.offer(root);
-    TreeLinkNode leftMostNode = root.left;
-    
-    while (leftMostNode != null){
-      for (TreeLinkNode node : list){
-        newList.offer(node.left);
-        newList.offer(node.right);
-      }
-      connectNodes(list);
-      list = newList;
-      newList = new LinkedList<TreeLinkNode>();
-      leftMostNode = list.peek().left;
-    }
-    connectNodes(list);
-  }
-  
-  private void connectNodes(LinkedList<TreeLinkNode> list){
-    if (list.size() == 1){
-      TreeLinkNode node = list.poll();
-      node.next = null;
-      return;
-    }
-    TreeLinkNode pre = list.poll();
-    pre.next = list.peek();
-    connectNodes(list);
+    connect(root.left);
+    connect(root.right);
   }
 
 }
