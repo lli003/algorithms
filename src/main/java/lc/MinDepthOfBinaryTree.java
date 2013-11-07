5916126cd55c927538e3a2dd1d33ea0e4384f76b
Lei Li <lli003@cs.fiu.edu>
2013-11-07 10:40:56 -0500

    add Min Depth of Binary Tree (recursive solution)

---
+++ src/main/java/lc/MinDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package lc;
+
+import lc.util.TreeNode;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ * @author leili
+ *
+ */
+public class MinDepthOfBinaryTree {
+
+  /**
+   * recursive solution
+   * @param root
+   * @return
+   */
+  public int minDepth(TreeNode root) {
+    if (root == null)
+      return 0;
+    if (root.left == null && root.right == null)
+      return 1;
+    else if (root.left == null && root.right != null)
+      return minDepth(root.right) + 1;
+    else if (root.left != null && root.right == null)
+      return minDepth(root.left) + 1;
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  }
+
+}
