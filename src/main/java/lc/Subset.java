5c631dcc616ca540bf0030a5f3b61f61c566c0d6
Lei Li <lli003@cs.fiu.edu>
2013-11-15 16:02:44 -0500

    add subset (indicator vector)

--- src/main/java/lc/Subset.java
+++ src/main/java/lc/Subset.java
@@ -5,28 +5,23 @@ import java.util.Arrays;
 
 /**
  * Given a set of distinct integers, S, return all possible subsets.
-
-Note:
-Elements in a subset must be in non-descending order.
-The solution set must not contain duplicate subsets.
-For example,
-If S = [1,2,3], a solution is:
-
-[
-  [3],
-  [1],
-  [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
-  [1,2],
-  []
-]
+ * 
+ * Note: Elements in a subset must be in non-descending order. The solution set
+ * must not contain duplicate subsets. For example, If S = [1,2,3], a solution
+ * is:
+ * 
+ * [ [3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], [] ]
+ * 
  * @author leili
- *
+ * 
  */
 public class Subset {
 
+  /**
+   * if not reaching the end of array, then do dfs
+   * @param S
+   * @return
+   */
   public ArrayList<ArrayList<Integer>> subsets(int[] S) {
     ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     if (S == null || S.length == 0)
@@ -47,6 +42,38 @@ public class Subset {
         subsetHelper(i + 1, S, result, ret);
       ret.remove(ret.size() - 1);
     }
+  }
+
+  /**
+   * using a vector of indicator to label visited position
+   * @param S
+   * @return
+   */
+  public ArrayList<ArrayList<Integer>> subsets2(int[] S) {
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    if (S == null || S.length == 0)
+      return result;
+    Arrays.sort(S);
+    boolean[] visit = new boolean[S.length];
+    subsetHelper2(0, S, result, visit);
+    return result;
+  }
+
+  private void subsetHelper2(int idx, int[] S,
+      ArrayList<ArrayList<Integer>> result, boolean[] visit) {
+    if (idx == S.length) {
+      ArrayList<Integer> ret = new ArrayList<Integer>();
+      for (int i = 0; i < visit.length; i++) {
+        if (visit[i])
+          ret.add(S[i]);
+      }
+      result.add(ret);
+      return;
+    }
+    visit[idx] = true;
+    subsetHelper2(idx + 1, S, result, visit);
+    visit[idx] = false;
+    subsetHelper2(idx + 1, S, result, visit);
   }
 
 }
