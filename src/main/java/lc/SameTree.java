1acd538b430c7d48fb7cfa93dbef06616692147c
lli003 <lli003@cs.fiu.edu>
2013-12-02 13:39:47 -0500

    improve lc sametree (iterative solution)

--- src/main/java/lc/SameTree.java
+++ src/main/java/lc/SameTree.java
@@ -1,5 +1,7 @@
 package lc;
 
+import java.util.Stack;
+
 import lc.util.TreeNode;
 
 /**
@@ -18,6 +20,37 @@ public class SameTree {
     if (p.val != q.val)
       return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+  
+  /**
+   * iterative solution
+   * @param p
+   * @param q
+   * @return
+   */
+  public boolean isSameTree2(TreeNode p, TreeNode q){
+    Stack<TreeNode> pstack = new Stack<TreeNode>();
+    Stack<TreeNode> qstack = new Stack<TreeNode>();
+    pstack.push(p);
+    qstack.push(q);
+    
+    while(!pstack.isEmpty() && !qstack.isEmpty()){
+        p = pstack.pop();
+        q = qstack.pop();
+        if (p != null && q != null && p.val == q.val){
+            pstack.push(p.right);
+            pstack.push(p.left);
+            qstack.push(q.right);
+            qstack.push(q.left);
+        } else if (p == null && q == null)
+            continue;
+        else
+            return false;
+    }
+    if (!pstack.isEmpty() || !qstack.isEmpty())
+        return false;
+    
+    return true;
   }
 }
 
