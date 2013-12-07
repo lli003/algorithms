6057a3a380de72145b5480b0cacde2b845d05221
Lei Li <lli003@cs.fiu.edu>
2013-12-06 21:37:16 -0500

    add basic parity of a bit sequence

---
+++ src/main/java/basic/bit/Parity.java
@@ -0,0 +1,40 @@
+package basic.bit;
+
+/**
+ * the parity of a sequence of bits is 1 if the number of 1s in the sequence is odd, or 0 otherwise
+ * @author leili
+ *
+ */
+public class Parity {
+  
+  /**
+   * count the number of 1s in a number
+   * @param num
+   * @return
+   */
+  public static short parity(long num){
+    short r = 0;
+    while (num != 0){
+      r ^= (num & 1);
+      num >>= 1;
+    }
+    return r;
+  }
+  
+  public static short parity2(long num){
+    short r = 0;
+    while (num != 0){
+      r ^= 1;
+      num &= (num-1);
+    }
+    return r;
+  }
+  
+  public static void main(String[] args){
+    System.out.println(Parity.parity(1));
+    System.out.println(Parity.parity(2));
+    System.out.println(Parity.parity2(3));
+    System.out.println(Parity.parity2(16));
+  }
+
+}
