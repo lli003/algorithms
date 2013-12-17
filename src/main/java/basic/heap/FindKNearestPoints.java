c88cd86a63f54b2923ef46ff3b651db32545410f
Lei Li <lli003@cs.fiu.edu>
2013-12-17 00:58:30 -0500

    modified desc

--- src/main/java/basic/heap/FindKNearestPoints.java
+++ src/main/java/basic/heap/FindKNearestPoints.java
@@ -3,6 +3,50 @@ package basic.heap;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+/**
+ * find k nearest points to the origin
+ * basic idea: use a max heap to the points
+ * @author leili
+ *
+ */
+public class FindKNearestPoints {
+  
+  public Point[] findKNearestPoints(Point[] points, int k){
+    PriorityQueue<Point> queue = new PriorityQueue<Point>(k, new PointComparator());
+    Point max = new Point(0,0);
+    for (Point p : points){
+      if (p.compareTo(max) > 0){
+        max = p;
+        if (queue.size() < k)
+          queue.add(p);
+      } else {
+        queue.add(p);
+      }
+    }
+    
+    Point[] closest = new Point[k];
+    for (int i = 0; i < k; i++){
+      closest[i] = queue.remove();
+    }
+    
+    return closest;
+  }
+  
+  public static void main(String[] args){
+    int n = 50;
+    Point[] points = new Point[n];
+    for (int i = n-1; i >= 0; i--){
+      points[i] = new Point(i,i);
+    }
+    
+    FindKNearestPoints fn = new FindKNearestPoints();
+    Point[] res = fn.findKNearestPoints(points, 10);
+    for (Point p : res)
+      System.out.println(p.toString());
+  }
+
+}
+
 class Point implements Comparable<Point>{
 
   private int x;
@@ -40,42 +84,4 @@ class PointComparator implements Comparator<Point>{
     return o1.compareTo(o2);
   }
   
-}
-
-public class FindKNearestPoints {
-  
-  public Point[] findKNearestPoints(Point[] points, int k){
-    PriorityQueue<Point> queue = new PriorityQueue<Point>(k, new PointComparator());
-    Point max = new Point(0,0);
-    for (Point p : points){
-      if (p.compareTo(max) > 0){
-        max = p;
-        if (queue.size() < k)
-          queue.add(p);
-      } else {
-        queue.add(p);
-      }
-    }
-    
-    Point[] closest = new Point[k];
-    for (int i = 0; i < k; i++){
-      closest[i] = queue.remove();
-    }
-    
-    return closest;
-  }
-  
-  public static void main(String[] args){
-    int n = 50;
-    Point[] points = new Point[n];
-    for (int i = n-1; i >= 0; i--){
-      points[i] = new Point(i,i);
-    }
-    
-    FindKNearestPoints fn = new FindKNearestPoints();
-    Point[] res = fn.findKNearestPoints(points, 10);
-    for (Point p : res)
-      System.out.println(p.toString());
-  }
-
 }
