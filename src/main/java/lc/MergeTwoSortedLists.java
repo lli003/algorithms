8ca9f3e5289bdaf1f1656ae3f9b7ae336849155e
Lei Li <lli003@cs.fiu.edu>
2014-01-03 15:19:32 -0500

    improved lc merge two sorted lists (constance space)

--- src/main/java/lc/MergeTwoSortedLists.java
+++ src/main/java/lc/MergeTwoSortedLists.java
@@ -39,4 +39,31 @@ public class MergeTwoSortedLists {
     return pre.next;
   }
   
+  /**
+   * constant space
+   * @param l1
+   * @param l2
+   * @return
+   */
+  public ListNode mergeTwoLists2(ListNode l1, ListNode l2){
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+    
+    ListNode c1 = l1.val <= l2.val ? l1 : l2;
+    ListNode c2 = l1.val <= l2.val ? l2 : l1;
+    while (c1.next != null && c2 != null){
+        if (c1.val <= c2.val && c2.val <= c1.next.val){
+            ListNode tmp = c2.next;
+            c2.next = c1.next;
+            c1.next = c2;
+            c1 = c1.next;
+            c2 = tmp;
+        } else
+            c1 = c1.next;
+    }
+    if (c1.next == null)
+        c1.next = c2;
+    return l1.val <= l2.val ? l1 : l2;
+  }
+  
 }
