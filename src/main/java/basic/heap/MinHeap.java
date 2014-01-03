9258246eea44f0ca3cddfef8feb10d697765c051
Lei Li <lli003@cs.fiu.edu>
2014-01-02 23:38:57 -0500

    minheap to be continued

--- src/main/java/basic/heap/MinHeap.java
+++ src/main/java/basic/heap/MinHeap.java
@@ -1,5 +1,32 @@
 package basic.heap;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MinHeap {
+  
+  public static void main(String[] args){
+    Comparator<Integer> comparator = new Comparator<Integer>(){
+      public int compare(Integer o1, Integer o2) {
+        if (o1.intValue() < o2.intValue())
+          return -1;
+        else if (o1.intValue() > o2.intValue())
+          return 1;
+        else
+          return 0;
+      }
+    };
+    
+    PriorityQueue<Integer> heap = new PriorityQueue<Integer>(5, comparator);
+    heap.add(5);
+    heap.add(4);
+    heap.add(3);
+    heap.add(2);
+    heap.add(1);
+    
+    while (!heap.isEmpty()){
+      System.out.println(heap.poll());
+    }
+  }
 
 }
