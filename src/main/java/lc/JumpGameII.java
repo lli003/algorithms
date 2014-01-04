f9989651a50651c440b334a0c2cd9d1db13ea13a
Lei Li <lli003@cs.fiu.edu>
2014-01-04 18:18:12 -0500

    add lc jump game II

---
+++ src/main/java/lc/JumpGameII.java
@@ -0,0 +1,25 @@
+package lc;
+
+public class JumpGameII {
+  
+  /**
+   * Start from slot i, we use max to track the farthest slot we can reach within one jump from i 
+   * and use next to track the next position where we need to jump.
+   * @param A
+   * @return
+   */
+  public int jump(int[] A){
+    int steps = 0;
+    for (int i=1, max=A[0], next=0; i<A.length; ++i) {
+        if (i>next) {
+            if (max < i)
+                return -1; // unreachable
+            next = max;
+            ++steps;
+        }
+        max = Math.max(max, i+A[i]);
+    }
+    return steps;
+  }
+
+}
