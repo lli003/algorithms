d65c36ebdaa2cadfb8aa4efd4ba750f8d4cad3c1
lli003 <lli003@cs.fiu.edu>
2014-01-08 15:07:13 -0500

    add cc150 chapter04 successor of a treenode

---
+++ src/main/java/cc150/chapter04/SuccessorOfTreeNode.java
@@ -0,0 +1,33 @@
+package cc150.chapter04;
+
+import lc.util.TreeNode;
+
+public class SuccessorOfTreeNode {
+
+  public TreeNode inorderSuccessor(TreeNode e) {
+    if (e != null) {
+
+      TreeNode suc;
+      if (e.parent == null || e.right != null) {
+        suc = leftMostChild(e.right);
+      } else {
+        while ((suc = e.parent) != null){
+          if (suc.left == e)
+            break;
+          e = suc;
+        }
+      }
+      return suc;
+    }
+    return null;
+  }
+
+  private TreeNode leftMostChild(TreeNode e) {
+    if (e == null)
+      return null;
+    while (e.left != null)
+      e = e.left;
+    return e;
+  }
+
+}
