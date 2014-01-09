37bf9c57657f51dc6c07316b4aefcdd4eb39b773
lli003 <lli003@cs.fiu.edu>
2014-01-09 17:23:47 -0500

    improve lc search in 2D matrix (O(m+n))

--- src/main/java/lc/Search2DMatrix.java
+++ src/main/java/lc/Search2DMatrix.java
@@ -61,4 +61,17 @@ public class Search2DMatrix {
     return false;
   }
   
+  public boolean searchMatrix2(int[][] matrix, int target){
+    int row = 0, col = matrix[0].length - 1;
+    while (row < matrix.length && col >= 0){
+        if (matrix[row][col] == target)
+            return true;
+        else if (matrix[row][col] < target)
+            row++;
+        else
+            col--;
+    }
+    return false;
+  }
+  
 }
